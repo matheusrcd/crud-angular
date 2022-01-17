d6b8ebfbbb0403a107264569b6ef1ffeef8fc853
Matheus Ricardo Tomas <matheusrcdtms@hotmail.com>
2022-01-17 13:47:12 -0300

    Created course entity and controller

--- crud-spring/src/main/java/com/matheus/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/matheus/crudspring/CrudSpringApplication.java
@@ -1,7 +1,12 @@
 package com.matheus.crudspring;
 
+import com.matheus.crudspring.model.Course;
+import com.matheus.crudspring.repository.CourseRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,4 +15,14 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDataBaseTest(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("front-end");
+			courseRepository.save(c);
+		};
+	}
 }
